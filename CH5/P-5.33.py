1479a89684515ef2811e6e1f6d5d5835e2cdf881
EUli16 <ethan.uliano@gmail.com>
2023-05-22 11:36:07 -0400

    finished P-5.33, moving to other problems

--- CH5/P-5.33.py
+++ CH5/P-5.33.py
@@ -1,28 +1,79 @@
-'''Write a python program for a matrix class that can add and multiply two-dimensional arrays of
-numbers. assuming the dimensions  agree appropriately  for the operation
-'''
-
 class Matrix:
 
-    def __init__(self, array):
-        self.row = len(array)#row is defined by the
-        self.column = len(array[0])#column is defined by the number of values in the first index of the list
-        self.matrix = array
+    def __init__(self, Array1):
+        self._array_1 = Array1
 
-    def get_size(self):
-        return self.row, self.column
 
-    def get_rows(self, i):
-        return self.matrix[i][:]
+    # def Add_2D_Array(self, Array1, Array2):
+    #     #see if row lengths match
+    #     if len(Array1) != len(Array2):
+    #         print('Array row lengths must match');
+    #         return
+    #     if len(Array1[0]) != len(Array2[0]):
+    #         print('Array column lengths must match');
+    #         return
+    #     sum = 0
+    #     #number of rows
+    #     for i in range(len(Array1)):
+    #         #number of columns(again assuming same matrix dimensions)
+    #         for j in range(len(Array1[0])):
+    #             #add each part of array1 and array2 to the sum variable
+    #             sum += Array1[i][j]
+    #             sum += Array2[i][j]
+    #
+    #     return sum
 
-    def get_columns(self, i):
-        return self.matrix#left off here, finish later had to leave
+    def __add__(self, Array2):
+        self._array_2 = Array2
+        # see if row lengths match
+        if len(self._array_1) != len(self._array_2):
+            print('Array row lengths must match');
+            return
+        if len(self._array_1[0]) != len(self._array_2[0]):
+            print('Array column lengths must match');
+            return
+        sum = 0
+        # number of rows
+        for i in range(len(self._array_1)):
+            # number of columns(again assuming same matrix dimensions)
+            for j in range(len(self._array_1[0])):
+                # add each part of array1 and array2 to the sum variable
+                sum += self._array_1[i][j]
+                sum += self._array_2[i][j]
+
+        return sum
+
+    def __mul__(self, Array2):
+        self._array_2 = Array2
+        # see if row lengths match
+        if len(self._array_1) != len(self._array_2):
+            print('Array row lengths must match');
+            return
+        if len(self._array_1[0]) != len(self._array_2[0]):
+            print('Array column lengths must match');
+            return
+        product = 1
+        # number of rows
+        for i in range(len(self._array_1)):
+            # number of columns(again assuming same matrix dimensions)
+            for j in range(len(self._array_1[0])):
+                # add each part of array1 and array2 to the sum variable
+                product *= self._array_1[i][j]
+                product *= self._array_2[i][j]
+
+        return product
+
+
+
 
 
 
 
 
 if __name__ == '__main__':
-    #proper way to initialize a 2d array in python is result = [ [0] * c for j in range(r) ]
-
-
+    data1 = [[1 , 2], [2,3], [3,4]]#15
+    data2 = [[2,3], [3,4], [4,5]]#21
+    matrix = Matrix(data1)
+    print(matrix + data2)
+    print(matrix * data2)
+    #print(matrix.Add_2D_Array(data1, data2))
